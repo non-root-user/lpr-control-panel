92684a178ea5091c26c1ca0e1451abd88b7785fe
BenekDoesHorses <b@horsi.fi>
2021-03-07 15:33:27 +0100

    Login error messages

--- main.py
+++ main.py
@@ -27,15 +27,17 @@ def login():
         plain_pass = request.form.get('password').encode('utf-8')
         hash_pass = bcrypt.hashpw(plain_pass, bcrypt.gensalt())
 
-        print("SELECT username, password FROM ponies WHERE username = '{}'".format(username))
         cur.execute("SELECT username, password FROM ponies WHERE username = '{}'".format(username))
         user_info = cur.fetchone()
         print(user_info)
         if user_info != None:
             test_pass = bcrypt.hashpw(plain_pass, user_info[1].encode('utf-8'))
             isIdentified = (user_info[1].encode('utf-8') == test_pass)
+            if not isIdentified:
+                return render_template('login.html', error="Password incorrect.")
             return '<h2>Username: {}<br>Hashed Pass: {}<br>Test Pass: {}<br>Is correct: {}</h2>'.format(username,hash_pass,test_pass,isIdentified)
         
-        return '<center><h1>Username: {}</h1><br><h1>Password: {}</h1></center>'.format(username, hash_pass)
+        #return '<center><h1>Username: {}</h1><br><h1>Password: {}</h1></center>'.format(username, hash_pass)
+        return render_template('login.html', error="There is user with that username.")
 
     return render_template('login.html')
