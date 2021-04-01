cfd5bb185408c2286e2fe2e1c9b7bd7563243e34
BenekDoesHorses <b@horsi.fi>
2021-04-02 00:56:27 +0200

    An API call, that enables full text searching for songs.

--- api/songs.py
+++ api/songs.py
@@ -1,4 +1,5 @@
 from helper import audit_log
+from flask import request
 
 
 def songs(app, session, db):
@@ -41,3 +42,27 @@ def songs(app, session, db):
                              "audio_format": song[4], "genre": song[5], "date_released": song[6], "album_name": song[7],
                              "fs_album_cover": song[8]}
             return {'songs':response}
+        return {'result': '401', 'message': 'Authentication failed'}, 401
+
+    @app.route('/api/songs/find/<search>', methods=['GET'])
+    def find_song(search):
+        if 'username' in session and (session['permissions'] & 1):
+            audit_log('tried song-searching for ' + search, session, request)
+            if len(search) < 3 or len(search) >= 64:
+                return {'result': '400', 'message': 'Invalid search length, must be between 3 and 64'}, 400
+            clean_search = db._cmysql.escape_string(search).decode()
+            cur = db.cursor()
+            query = '''SELECT * FROM songs WHERE 
+                CONCAT(artist,title,album_name) LIKE '{}' 
+                ORDER BY title ASC LIMIT 0,50;'''.format(clean_search)
+            cur.execute(query)
+            result = cur.fetchall()
+            response = []
+            for n in result:
+                response.append({"id": n[0], "artist": n[1], "title": n[2], "fs_filename": n[3],
+                                "audio_format": n[4], "genre": n[5], "date_released": n[6],
+                                 "album_name": n[7], "fs_album_cover": n[8]})
+            return {'songs':response}
+        return {'result': '401', 'message': 'Authentication failed'}, 401
+
+
