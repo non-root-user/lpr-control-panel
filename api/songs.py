04ec6f10eb86e7836d7ac7d34cab8d4ae6e070b4
non_root_user <b@horsi.fi>
2021-10-13 04:38:52 +0200

    Api call for changing cover images, or adding new when none present

--- api/songs.py
+++ api/songs.py
@@ -1,5 +1,6 @@
 from helper import audit_log
 from flask import request
+import base64
 
 
 def songs(app, session, db):
@@ -50,7 +51,7 @@ def songs(app, session, db):
             audit_log('tried song-searching for ' + search, session, request)
             if len(search) < 3 or len(search) >= 64:
                 return {'result': '400', 'message': 'Invalid search length, must be between 3 and 64'}, 400
-            clean_search = db._cmysql.escape_string(search).decode()
+            clean_search = db.converter.escape(search)
             cur = db.cursor()
             query = '''SELECT * FROM songs WHERE 
                 CONCAT(artist,title,album_name) LIKE '{}' 
@@ -65,7 +66,7 @@ def songs(app, session, db):
             return {'songs': response}
         return {'result': '401', 'message': 'Authentication failed'}, 401
 
-    @app.route('/api/songs/albumart/<song_id>', methods=['GET'])
+    @app.route('/api/song/albumart/<song_id>', methods=['GET'])
     def get_coverart(song_id):
         if 'username' in session and (session['permissions'] & 1):
             audit_log('tried getting the cover of ' + song_id, session, request)
@@ -80,4 +81,37 @@ def songs(app, session, db):
                 return {'result': '400', 'message': 'Song with that id does not exist, or the song has no cover art attached'}, 400
             return cover[1], 200, {'Content-Type': 'image/png'}
 
+        return {'result': '401', 'message': 'Authentication failed'}, 401
+
+    @app.route('/api/song/albumart/<song_id>', methods=['PUT'])
+    def change_coverart(song_id):
+        if 'username' in session and (session['permissions'] & 2) and (session['permissions'] & 1):
+            response = ast.literal_eval(request.data.decode())
+            try:
+                image = response['image']
+                image = base64.b64decode(image)
+            except:
+                audit_log('has tried to change the cover of ' + song_id, session, request)
+                return {'result': '400', 'message': 'Invalid image'}, 400
+            try:
+                song_id = int(song_id)
+            except:
+                audit_log('has tried to change the cover of ' + song_id, session, request)
+                return {'result': '400', 'message': 'Invalid id'}, 400
+            cur = db.cursor()
+            if not cur.execute("SELECT id FROM albumarts WHERE id = '{}';".format(song_id)):
+                if not cur.execute("SELECT id FROM songs WHERE id = '{}';".format(song_id)):
+                    audit_log('has tried to change the cover of ' + song_id, session, request)
+                    return {'result': '400', 'message': 'Song with that id does not exist'}, 400
+                else:
+                    cur.execute("INSERT INTO albumarts (id, image) VALUES';".format(song_id, image))
+                    db.commit()
+                    audit_log('has changed the default cover of ' + song_id, session, request)
+                    return {'result': '200', 'message': 'Cover updated succesfully, new row has been created'}
+            else:
+                cur.execute("UPDATE albumarts SET image = {} WHERE id = '{}';".format(image, song_id))
+                db.commit()
+                audit_log('has changed the cover of ' + song_id, session, request)
+                return {'result': '200', 'message': 'Cover updated succesfully!'}
+            
         return {'result': '401', 'message': 'Authentication failed'}, 401
