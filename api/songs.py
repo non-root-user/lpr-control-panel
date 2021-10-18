95f5acdebf0000b1c4086522c100df76f5ce0fac
non_root_user <b@horsi.fi>
2021-10-18 03:36:56 +0200

    Made code in songs.py compliant with PEP8 standard

--- api/songs.py
+++ api/songs.py
@@ -10,34 +10,34 @@ def songs(app, session, db):
 
     @app.route('/api/songs/<page>', methods=['GET'])
     def list_songs(page):
-        if 'username' in session and (session['permissions'] & 2) and (session['permissions'] & 1):
+        if ('username' in session and (session['permissions'] & 2)
+                and (session['permissions'] & 1)):
             try:
                 page = int(page)
-            except:
+            except ValueError:
                 page = 0
             cur = db.cursor()
             cur.execute("SELECT * FROM songs LIMIT {},5;".format(page * 15))
             result = cur.fetchall()
             response = []
             for n in result:
-                response.append({"id": n[0], "artist": n[1], "title": n[2], "fs_filename": n[3],
-                                "audio_format": n[4], "genre": n[5], "date_released": n[6], "album_name": n[7],
-                                 "fs_album_cover": n[8]})
+                response.append({"id": n[0], "artist": n[1], "title": n[2],
+                                 "fs_filename": n[3], "audio_format": n[4],
+                                 "genre": n[5], "date_released": n[6],
+                                 "album_name": n[7], "fs_album_cover": n[8]})
             return {'songs': response}
         return {'result': '401', 'message': 'Authentication failed'}, 401
-
 
     @app.route('/api/songs', methods=['GET'])
     def list_some_songs():
         return list_songs(0)
-
 
     @app.route('/api/song/<song_id>', methods=['GET'])
     def get_song(song_id):
         if 'username' in session and (session['permissions'] & 1):
             try:
                 song_id = int(song_id)
-            except:
+            except ValueError:
                 return {'result': '400', 'message': 'Invalid id'}, 400
 
             cur = db.cursor()
@@ -45,15 +45,16 @@ def songs(app, session, db):
             song = cur.fetchone()
 
             if not song:
-                return {'result': '400', 'message': 'Song with that id does not exist'}, 400
-            response = {"id": song[0], "artist": song[1], "title": song[2], "fs_filename": song[3],
-                        "audio_format": song[4], "genre": song[5], "date_released": song[6], "album_name": song[7],
-                        "fs_album_cover": song[8]}
+                return {'result': '400', 'message':
+                        'Song with that id does not exist'}, 400
+            response = {"id": song[0], "artist": song[1], "title": song[2],
+                        "fs_filename": song[3], "audio_format": song[4],
+                        "genre": song[5], "date_released": song[6],
+                        "album_name": song[7], "fs_album_cover": song[8]}
 
             return {'songs': response}
 
         return {'result': '401', 'message': 'Authentication failed'}, 401
-
 
     @app.route('/api/songs/find/<search>', methods=['GET'])
     def find_song(search):
@@ -61,7 +62,8 @@ def songs(app, session, db):
             audit_log('tried song-searching for ' + search, session, request)
 
             if len(search) < 3 or len(search) >= 64:
-                return {'result': '400', 'message': 'Invalid search length, must be between 3 and 64'}, 400
+                return {'result': '400', 'message':
+                        'Invalid search length, must be between 3 and 64'}, 400
 
             clean_search = db.converter.escape(search)
             cur = db.cursor()
@@ -73,18 +75,20 @@ def songs(app, session, db):
             response = []
 
             for n in result:
-                response.append({"id": n[0], "artist": n[1], "title": n[2], "fs_filename": n[3],
-                                "audio_format": n[4], "genre": n[5], "date_released": n[6],
+                response.append({"id": n[0], "artist": n[1], "title": n[2],
+                                 "fs_filename": n[3], "audio_format": n[4],
+                                 "genre": n[5], "date_released": n[6],
                                  "album_name": n[7], "fs_album_cover": n[8]})
 
             return {'songs': response}
 
         return {'result': '401', 'message': 'Authentication failed'}, 401
 
-
     @app.route('/api/song', methods=['POST'])
     def add_song():
-        if 'username' in session and (session['permissions'] & 2) and (session['permissions'] & 1):
+        if ('username' in session and (session['permissions'] & 2) and
+           (session['permissions'] & 1)):
+
             response = ast.literal_eval(request.data.decode())
 
             try:
@@ -94,42 +98,52 @@ def songs(app, session, db):
                 genre = db.converter.escape(response["genre"])
                 date_released = response["date_released"]
                 audio_file = base64.b64decode(response["audio_file"])
-                values = [title, artist, album_name, genre, date_released, audio_file]
-            except:
-                return {'result': '400', 'message': 'Invalid request values'}, 400
+                values = [title, artist, album_name, genre,
+                          date_released, audio_file]
+            except Exception:
+                return {'result': '400', 'message':
+                        'Invalid request values'}, 400
 
             for x in values:
                 if not x:
-                    return {'result': '400', 'message': 'Provided empty request values'}, 400
+                    return {'result': '400', 'message':
+                            'Provided empty request values'}, 400
 
-            filename = ",".join([artist, album_name, title + ".mp3"]).replace(" ", "_")
+            filename = ",".join([artist, album_name,
+                                 title + ".mp3"]).replace(" ", "_")
             if os.path.isfile(Config.song_path + filename):
-                return {'result': '409', 'message': 'These song values are already occupied'}, 409
+                return {'result': '409', 'message':
+                        'These song values are already occupied'}, 409
 
             file = open(Config.song_path + filename, 'wb+')
             file.write(audio_file)
             file.close()
             cur = db.cursor()
-            query_arr = [artist, title, filename, 'mp3', genre, date_released, album_name, 'placeholder']
-            cur.execute('''INSERT INTO songs 
+            query_arr = [artist, title, filename, 'mp3', genre, date_released,
+                         album_name, 'placeholder']
+            cur.execute('''INSERT INTO songs
                 (artist,title,fs_filename,audio_format,genre,date_released,album_name,fs_album_cover)
-                VALUES ('{}','{}','{}','{}','{}','{}','{}','{}')'''.format(*query_arr))
+                VALUES ('{}','{}','{}',
+                        '{}','{}','{}','{}','{}')'''.format(*query_arr))
             db.commit()
             cur.execute('SELECT LAST_INSERT_ID();')
             new_id = cur.fetchone()
-            audit_log('added a song with id:' + ''.join(str(x) for x in new_id), session, request)
+            audit_log('added a song with id:' + ''.join(
+                str(x) for x in new_id), session, request)
 
-            return {'result': '201', 'message': 'Song added succesfully', 'song_id': new_id}, 201
+            return {'result': '201', 'message': 'Song added succesfully',
+                    'song_id': new_id}, 201
 
         return {'result': '401', 'message': 'Authentication failed'}, 401
 
-    
     @app.route('/api/song/<song_id>', methods=['DELETE'])
     def delete_song(song_id):
-        if 'username' in session and (session['permissions'] & 2) and (session['permissions'] & 1):
+        if ('username' in session and (session['permissions'] & 2) and
+           (session['permissions'] & 1)):
+
             try:
                 song_id = int(song_id)
-            except:
+            except ValueError:
                 return {'result': '400', 'message': 'Invalid id'}, 400
 
             cur = db.cursor()
@@ -137,21 +151,23 @@ def songs(app, session, db):
             song = cur.fetchone()
 
             if not song:
-                return {'result': '400', 'message': 'Song with that id does not exist'}, 400
-            
+                return {'result': '400', 'message':
+                        'Song with that id does not exist'}, 400
+
         cur.execute("DELETE FROM songs WHERE id = {}".format(song_id))
         cur.execute("DELETE FROM albumarts WHERE id = {}".format(song_id))
         db.commit()
 
         return {'result': '401', 'message': 'Authentication failed'}, 401
 
-
     @app.route('/api/song/<song_id>', methods=['PUT'])
     def edit_song_info(song_id):
-        if 'username' in session and (session['permissions'] & 2) and (session['permissions'] & 1):
+        if ('username' in session and (session['permissions'] & 2) and
+           (session['permissions'] & 1)):
+
             try:
                 song_id = int(song_id)
-            except:
+            except ValueError:
                 return {'result': '400', 'message': 'Invalid id'}, 400
 
             response = ast.literal_eval(request.data.decode())
@@ -159,12 +175,15 @@ def songs(app, session, db):
             song = cur.fetchone()
 
             if not song:
-                return {'result': '400', 'message': 'Song with that id does not exist'}, 400
+                return {'result': '400', 'message':
+                        'Song with that id does not exist'}, 400
             if not response:
-                return {'result': '400', 'message': 'No values to change were given'}, 400
-            
+                return {'result': '400', 'message':
+                        'No values to change were given'}, 400
+
             try:
-                allowed_keys = ['title','artist','album_name','genre','date_released','audio_file']
+                allowed_keys = ['title', 'artist', 'album_name', 'genre',
+                                'date_released', 'audio_file']
                 query = "UPDATE songs SET "
                 changes = []
 
@@ -194,10 +213,12 @@ def songs(app, session, db):
 
             except IOError as err:
                 audit_log("Could not save a file when updating song info: {}".format(str(err)), session, request)
-                return {'result': '500', 'message': 'The server encountered an error when saving the song file'}, 500
+                return {'result': '500', 'message':
+                        'The server encountered an error when saving the song file'}, 500
             except Exception as err:
                 audit_log(str(err), session, request)
-                return {'result': '400', 'message': 'Request values are not permissible'}, 400
+                return {'result': '400', 'message':
+                        'Request values are not permissible'}, 400
 
     # TODO fuzzy song search
     # TODO audio file check, and proper reincoding according to the config
