e14d1952faa973c68ef34abac39737fd5a53a683
non_root_user <b@horsi.fi>
2021-10-24 04:38:06 +0200

    First version of the fuzzy song search algorithm

--- api/songs.py
+++ api/songs.py
@@ -84,6 +84,38 @@ def songs(app, session, db):
 
         return {'result': '401', 'message': 'Authentication failed'}, 401
 
+    @app.route('/api/songs/fuzzy/<search>', methods=['GET'])
+    def fuzzy_song(search):
+        if 'username' in session and (session['permissions'] & 1):
+
+            if len(search) < 3 or len(search) >= 64:
+                return {'result': '400', 'message':
+                        'Invalid search length, must be between 3 and 64'}, 400
+
+            clean_search = db.converter.escape(search)
+            cur = db.cursor()
+            query = """SELECT * FROM songs WHERE (SOUNDEX(artist) LIKE
+                       CONCAT('%', TRIM(TRAILING '0' FROM
+                       SOUNDEX('{0}')), '%')) OR
+                       (SOUNDEX(title) LIKE CONCAT('%', TRIM(TRAILING '0'
+                       FROM SOUNDEX('{0}')), '%')) OR
+                       (SOUNDEX(album_name) LIKE CONCAT('%',
+                       TRIM(TRAILING '0' FROM SOUNDEX('{0}')), '%'))
+                       ORDER BY title ASC LIMIT 0,50;""".format(clean_search)
+            cur.execute(query)
+            result = cur.fetchall()
+            response = []
+
+            for n in result:
+                response.append({"id": n[0], "artist": n[1], "title": n[2],
+                                 "fs_filename": n[3], "audio_format": n[4],
+                                 "genre": n[5], "date_released": n[6],
+                                 "album_name": n[7], "fs_album_cover": n[8]})
+
+            return {'songs': response}
+
+        return {'result': '401', 'message': 'Authentication failed'}, 401
+
     @app.route('/api/song', methods=['POST'])
     def add_song():
         if ('username' in session and (session['permissions'] & 2) and
@@ -235,9 +267,9 @@ def songs(app, session, db):
 
     # TODO fuzzy song search
     # TODO audio file check, and proper reincoding according to the config
-    # TODO edit song info
     # TODO verbose audit log for deleting and editing
     # TODO error log as a separate function and a separate file
     # TODO separate access log, for things like getting a cover
     # TODO an api call for a song preview, make ffmpeg generate a short sample
     #      from the middle of a song, and serve it as a stream
+    # TODO separate log management panel for admins
